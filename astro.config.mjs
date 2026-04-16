c760b907e7a06491469064c523c3523a0b41d552
danh.thai <ronwindy@gmail.com>
2026-04-16 23:26:25 +0700

    add deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ronwindy.github.io',
+  base: '/persona-4-golden-guide',
+});
