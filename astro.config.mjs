14d8cb94507ef2ad6eb3490c05dfd3fc9d716350
danh.thai <ronwindy@gmail.com>
2026-09-19 15:02:18 +0700

    add astro:icon to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ronwindy.github.io',
   base: '/persona-4-golden-guide',
+  integrations: [icon()],
 });
---
+++ src/icons/README.md
@@ -0,0 +1,7 @@
+# Local Custom Icons
+
+Place any custom `.svg` files in this directory to use them with `astro-icon`:
+
+```astro
+<Icon name="my-custom-icon" />
+```
